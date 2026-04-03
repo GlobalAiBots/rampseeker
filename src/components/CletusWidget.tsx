def31a696f57fc6fd23ad06850472f2d01725c34
GlobalAIBots <james.lee.mull@gmail.com>
2026-04-03 16:22:30 -0500

    Connect production CLETUS widget with dedicated RampSeeker Guide agent - 82 Q&A pairs

--- src/components/CletusWidget.tsx
+++ src/components/CletusWidget.tsx
@@ -5,7 +5,8 @@ import { useEffect } from "react";
 export default function CletusWidget() {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://www.globalaibots.com/widget-demo.js";
+    script.src = "https://www.globalaibots.com/widget.js";
+    script.setAttribute("data-id", "fe1aa7b6-f745-4c92-b353-2589e2599bc3");
     script.setAttribute("data-agent-name", "RampSeeker Guide");
     script.setAttribute("data-color", "#1E6091");
     script.defer = true;
